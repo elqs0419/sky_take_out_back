325fa039092a899a27e7814f4cb07f21543cff3e
elqs-19 <2496792431@qq.com>
2025-05-20 20:21:06 +0800

    新增员工业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -80,6 +80,7 @@ public class EmployeeController {
     @PostMapping
     public Result save(@RequestBody EmployeeDTO employeeDTO){
         log.info("新增员工：{}",employeeDTO);
+        System.out.println("当前线程ID" + Thread.currentThread().getId());
         employeeService.save(employeeDTO);
         return Result.success();
     }
--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.sky.service.impl;
 import com.sky.constant.MessageConstant;
 import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
@@ -66,6 +67,7 @@ public class EmployeeServiceImpl implements EmployeeService {
      */
     @Override
     public void save(EmployeeDTO employeeDTO) {
+        System.out.println("当前线程ID" + Thread.currentThread().getId());
         Employee employee = new Employee();
         //对象属性拷贝(原始对象，目标对象)
         BeanUtils.copyProperties(employeeDTO,employee);
@@ -77,9 +79,9 @@ public class EmployeeServiceImpl implements EmployeeService {
         employee.setCreateTime(LocalDateTime.now());
         employee.setUpdateTime(LocalDateTime.now());
         //设置创建人id和修改人id
-        // TODO 后期需要改为当前登陆用户id
-        employee.setCreateUser(10L);
-        employee.setUpdateUser(10L);
+
+        employee.setCreateUser(BaseContext.getCurrentId());
+        employee.setUpdateUser(BaseContext.getCurrentId());
 
         employeeMapper.insert(employee);
 
