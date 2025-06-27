5d3de101207d7ddd13f53283bb13308833cff2ad
elqs-19 <2496792431@qq.com>
2025-06-27 15:14:50 +0800

    缓存套餐代码开发

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -9,9 +9,11 @@ import com.sky.service.DishService;
 import com.sky.vo.DishVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * 菜品管理
@@ -23,7 +25,8 @@ public class DishController {
 
     @Autowired
     private DishService dishService;
-
+    @Autowired
+    private RedisTemplate redisTemplate;
     /**
      * 新增菜品
      * @param dishDTO
@@ -33,6 +36,9 @@ public class DishController {
     public Result save(@RequestBody DishDTO dishDTO) {
         log.info("新增菜品：{}", dishDTO);
         dishService.saveWithFlavor(dishDTO);
+        //清理缓存数据
+        String key = "dish_" + dishDTO.getCategoryId();
+        cleanCache(key);
         return Result.success();
     }
 
@@ -57,7 +63,8 @@ public class DishController {
     public Result delete(@RequestParam List<Long> ids){
         log.info("删除菜品:{}",  ids);
         dishService.deleteBatch(ids);
-
+        //清理redis缓存
+        cleanCache("dish_*");
         return Result.success();
     }
 
@@ -83,7 +90,8 @@ public class DishController {
     public Result update(@RequestBody DishDTO dishDTO){
         log.info("修改菜品：{}",dishDTO);
         dishService.updateWithFlavor(dishDTO);
-
+        //清理redis缓存
+        cleanCache("dish_*");
         return Result.success();
     }
 
@@ -97,6 +105,8 @@ public class DishController {
     public Result setStatus(@PathVariable Integer status, Long id){
         log.info("菜品起售停售：{},{}",status,id);
         dishService.startOrStop(status,id);
+        //清理redis缓存
+        cleanCache("dish_*");
         return Result.success();
     }
 
@@ -113,6 +123,9 @@ public class DishController {
     }
 
 
-
+    private void cleanCache(String pattern){
+        Set keys = redisTemplate.keys(pattern);
+        redisTemplate.delete(keys);
+    }
 
 }
--- sky-server/src/main/java/com/sky/controller/admin/SetMealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetMealController.java
@@ -8,6 +8,7 @@ import com.sky.service.SetMealService;
 import com.sky.vo.SetmealVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.interfaces.RSAKey;
@@ -30,6 +31,7 @@ public class SetMealController {
      * @return
      */
     @PostMapping
+    @CacheEvict(cacheNames = "setmealCache",key = "#setmealDTO.categoryId")
     public Result save(@RequestBody SetmealDTO setmealDTO){
         log.info("套餐新增：{}",setmealDTO);
         setMealService.saveWithDish(setmealDTO);
@@ -54,6 +56,7 @@ public class SetMealController {
      * @return
      */
     @DeleteMapping
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)
     public Result delete(@RequestParam List<Long> ids){
         log.info("批量删除套餐，ids：{}",ids);
         setMealService.deleteBatch(ids);
@@ -79,6 +82,7 @@ public class SetMealController {
      * @return
      */
     @PutMapping
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)
     public Result update(@RequestBody SetmealDTO setmealDTO){
         log.info("修改套餐：{}",setmealDTO);
         setMealService.updateWithDish(setmealDTO);
@@ -92,6 +96,7 @@ public class SetMealController {
      * @return
      */
     @PostMapping("/status/{status}")
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)
     public Result startOrStop(@PathVariable Integer status, Long id){
         log.info("套餐起售停售：{},{}",status,id);
         setMealService.startOrStop(status,id);
